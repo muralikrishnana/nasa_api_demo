648d299d9f5662d33e1ca5fbf4558ec9e4dc5d13
Murali Krishnan A <amuralikrishnan@outlook.com>
2023-01-18 14:45:10 +0530

    Add infinite scrolling.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { AppHeader } from '@/src/components/AppHeader';
+import { useWindowScroll } from '@mantine/hooks';
 import moment from 'moment';
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import { TodayAPOD } from '../components/TodayAPOD';
-
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { APODsDisplay } from '../components/APODsDisplay';
+import { TodayAPOD } from '../components/TodayAPOD';
 import { NASA_API_COMPLIANT_DATE_FORMAT } from '../misc/constants';
 import { APOD_Request_Data, NASA_API_Response, Success_Based_Response } from '../types/nasa-api';
 
@@ -18,6 +18,64 @@ async function fetchAPODs(props: APOD_Request_Data): Promise<Success_Based_Respo
 export default function Home() {
   const [apods, setApods] = useState<NASA_API_Response[]>([]);
   const [apodLoading, setApodLoading] = useState<boolean>(false);
+
+  const oldestAPODRef = useRef<NASA_API_Response | null>();
+  const apodLoadingRef = useRef<boolean>();
+
+  const [scroll, scrollTo] = useWindowScroll();
+
+  useEffect(() => {
+    if (apods.length < 1) oldestAPODRef.current = null;
+    else oldestAPODRef.current = apods[apods.length - 1];
+  }, [apods]);
+
+  useEffect(() => {
+    apodLoadingRef.current = apodLoading;
+  }, [apodLoading]);
+
+  const loadMoreAPODs = useCallback(async () => {
+    if (apodLoadingRef.current) return [];
+
+    if (!oldestAPODRef.current) return [];
+
+    const lastLoadedDateMinus1Day = moment(oldestAPODRef.current.date)
+      .subtract(1, 'day')
+      .format(NASA_API_COMPLIANT_DATE_FORMAT);
+    const startDate = moment(lastLoadedDateMinus1Day).subtract(11, 'days').format(NASA_API_COMPLIANT_DATE_FORMAT);
+
+    setApodLoading(true);
+
+    const moreAPODsResponse = await fetchAPODs({
+      startDate,
+      endDate: lastLoadedDateMinus1Day,
+    });
+
+    setApodLoading(false);
+
+    if (moreAPODsResponse.success && Array.isArray(moreAPODsResponse.data) && moreAPODsResponse.data.length > 0) {
+      return moreAPODsResponse.data;
+    }
+
+    return [];
+  }, []);
+
+  useEffect(() => {
+    // infinite loading
+    const totalHeightOfPage = document.body.scrollHeight;
+    const shouldLoadMore = totalHeightOfPage - scroll.y - window.innerHeight < 0;
+
+    if (!shouldLoadMore) return;
+
+    (async () => {
+      const moreAPODs = await loadMoreAPODs();
+
+      if (moreAPODs.length < 1) return;
+
+      setApods((apods) => {
+        return [...apods, ...moreAPODs.reverse()];
+      });
+    })();
+  }, [loadMoreAPODs, scroll]);
 
   useEffect(() => {
     (async () => {
